001ffacb4903b458445c7c18122e3f9ceaa9861a
Bredfen <game_c_v@mail.ru>
2025-05-20 06:41:29 +0300

    Update Databases.kt

--- src/main/kotlin/Databases.kt
+++ src/main/kotlin/Databases.kt
@@ -81,7 +81,7 @@ fun Application.configureDatabases() {
 private const val MONGO_DATABASE_NAME = "myDatabase"
 
 fun Application.connectToMongoDB(): MongoDatabase {
-    val uri = "mongodb+srv://bredfen:yEQtS6Cj9vfhlIJr@cluster0.vyzlr8s.mongodb.net/"
+    val uri = "mongodb+srv://bredfen:yEQtS6Cj9vfhlIJr@cluster0.vyzlr8s.mongodb.net/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.0"
 
     println("Connecting to MongoDB at URI: $uri")
     val mongoClient = MongoClients.create(uri)
