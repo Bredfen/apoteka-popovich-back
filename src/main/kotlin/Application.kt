fc49c33c6123b4563fc7f9b4825f0a070549f9d5
Bredfen <game_c_v@mail.ru>
2025-05-20 08:22:11 +0300

    Update Application.kt

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -5,18 +5,14 @@ import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 
 fun main() {
-    embeddedServer(CIO, port = 80, host = "0.0.0.0", module = Application::module)
+    embeddedServer(CIO, port = 80, host = "localhost", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
-
         configureSecurity()
         configureSerialization()
         configureHTTP()
         configureDatabases()
         configureRouting()
-
-
-
 }
